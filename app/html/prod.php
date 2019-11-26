2e56112a7a3c866c35f4db3d5ce40d24872c443b
Vassiliy Yegorov <vasyakrg@gmail.com>
2019-11-26 12:10:27 +0700

    fix settings

--- app/html/prod.php
+++ app/html/prod.php
@@ -2,14 +2,14 @@
 
 $projects=array(
     'antiage' => '11010',
-    'avaglobals' => '11110',
+    'avaglobals' => '11010',
     'demo' => '11111',
     'effectrehab' => '11110',
-    'ekomag' => '11110',
-    'kgoal' => '11110',
-    'molodost' => '11110',
-    'newera' => '11110',
-    'taomed' => '11111'
+    'ekomag' => '11010',
+    'kgoal' => '11010',
+    'molodost' => '11010',
+    'newera' => '11010',
+    'taomed' => '11011'
 );
 
 $ns='Prod';
